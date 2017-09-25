bd18caab6f760b6857d3434b48604e3a68c02aa7
manish2aug <manish2aug@gmail.com>
2017-09-25 19:41:32 +0200

    Added source

--- src/app/models/teamRanking.ts
+++ src/app/models/teamRanking.ts
@@ -1,6 +1,7 @@
 import {Score} from "./score";
 export class TeamRanking {
   constructor(public name:string,
-              public points:string) {
+              public points:string,
+              public pointBalance:string) {
   }
 }
