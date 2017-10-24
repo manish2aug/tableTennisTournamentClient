93b50f0172017e7c2fc9ab6875c0a5451f738276
manish2aug <manish2aug@gmail.com>
2017-10-24 06:54:32 +0200

    Added source

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,40 @@ import {SingleGameComponent} from "./singlematch-details/singlematch-details.com
 import {TeamRankingComponent} from "./team-rankings/team-rankings.component";
 import {IndividualRankingComponent} from "./individual-rankings/individual-rankings.component";
 import {ScoreRecordingComponent} from "./score-recording/score-recording.component";
-
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule,
+} from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,9 +55,14 @@ import {ScoreRecordingComponent} from "./score-recording/score-recording.compone
     BrowserModule,
     FormsModule,
     HttpModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/constants/applicationConstants.ts
+++ src/app/constants/applicationConstants.ts
@@ -2,6 +2,6 @@ export class ApplicationConstants {
   public static HOST_IP:string = 'localhost';
   public static SERVER_PORT:string = '8080';
   // public static SERVER_PORT:string = '80';
-  // public static HOST_IP:string = '105.229.162.182';
+  // public static HOST_IP:string = '105.186.182.19';
   public static SERVICE_ENDPOINT = 'http://' + ApplicationConstants.HOST_IP + ':' + ApplicationConstants.SERVER_PORT;
 }
